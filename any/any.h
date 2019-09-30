38ff5c5c6cedb2bb4ab5192c9a0982fc59a5c768
Marius Tomescu <marius.tomescu@mindmaze.ch>
2019-09-30 22:28:49 +0300

    tring

--- any/any.h
+++ any/any.h
@@ -12,6 +12,7 @@
 #include <utility>
 #include <initializer_list>
 #include <type_traits>
+#include <variant>
 
 class bad_any_cast : public std::bad_cast
 {
@@ -25,10 +26,23 @@ constexpr size_t small_space_size = 4 * sizeof(void*);
 
 template<class T>
 using any_is_trivial = std::bool_constant<std::is_trivially_copyable_v<T> // Check if alignment is necessary
-									   && sizeof(T) <= small_space_size>;
+										&& sizeof(T) <= small_space_size
+										&& alignof(T) <= alignof(void*)>;
 
 template<class T>
-using any_is_small = std::bool_constant<sizeof(T) <= small_space_size>; // Check if alignment is necessary
+using any_is_small = std::bool_constant<sizeof(T) <= small_space_size
+									 && alignof(T) <= alignof(void*)>; // Check if alignment is necessary
+
+typedef std::aligned_storage_t<small_space_size, std::alignment_of_v<void*>> internal_storage_t;
+
+struct storage
+{
+	typedef std::aligned_storage_t<small_space_size, std::alignment_of_v<void*>> internal_storage_t;
+
+	void* big_storage = nullptr;
+	internal_storage_t small_storage;
+	internal_storage_t trivial_storage;
+};
 
 enum class any_representation
 {
@@ -57,17 +71,102 @@ struct any_big
 
 struct any_small
 {
+	template <class T>
+	static void Destroy(void* target) noexcept
+	{
+		std::destroy_at<T>(target);
+	}
+
+	template<class T>
+	static void Copy(const void* destination, void* what)
+	{
+		Construct(*static_cast<T*>(destination), *static_cast<T*>(what));
+	}
+
+	template<class T>
+	static void Move(const void* destination, void* what)
+	{
+		Construct(*static_cast<T*>(destination), std::move(*static_cast<T*>(what)));
+	}
 
 	void (*_destroy)(void*);
 	void (*_copy)(const void*);
 	void (*_move)(void*);
 };
 
-struct any_small
-{
+template<class T>
+any_big any_big_obj = {&any_big::Destroy<T>, &any_big::Copy<T>};
 
-};
+template<class T>
+any_small any_small_obj = {&any_small::Destroy<T>, &any_small::Copy<T>, &any_small::Move<T>};
 
 class any
 {
+public:
+	constexpr any() noexcept
+	:_storage{},
+	 _representation{}
+	{
+	}
+
+	any(const any& other)
+	:_storage{},
+	 _representation{}
+	{
+		if (!other.has_value())
+		{
+			return;
+		}
+
+		switch (other._representation)
+		{
+			case any_representation::Big:
+				_storage = static_cast<void*>(_aligned_malloc(sizeof(other.type()), alignof(other.type())));
+				break;
+			case any_representation::Small:
+				break;
+			case any_representation::Trivial:
+				break;
+		}
+	}
+	any(any&& other) noexcept;
+
+	template<class T>
+	any(T&& value) = delete;
+
+	template<class T, class... Args>
+	explicit any(std::in_place_type_t<T>, Args&& ...) = delete;
+	template<class T, class U, class...Args>
+	explicit any(std::in_place_type_t<T>, std::initializer_list<U>, Args&& ...) = delete; //might not do
+
+	~any()
+	{
+
+	}
+
+	any& operator=(const any& rhs);
+	any& operator=(any&& rhs) noexcept;
+	template<class T>
+	any& operator=(T&& rhs) = delete;
+
+	template<class T, class... Args>
+	std::decay_t<T>& emplace(Args&& ...) = delete;
+	template<class T, class U, class... Args>
+	std::decay_t<T>& emplace(std::initializer_list<U>, Args&& ...) = delete;
+	void reset() noexcept = delete;
+	void swap(any& rhs) noexcept = delete;
+
+	bool has_value() const noexcept;
+	const std::type_info& type() const noexcept;
+
+private:
+	std::variant<internal_storage_t, void*> _storage;
+	any_representation _representation;
+
 };
+
+template<class T, class... Args>
+void Construct(T& destination, Args&& ... args)
+{
+	new(destination)T(std::forward<Args>(args)...);
+}
